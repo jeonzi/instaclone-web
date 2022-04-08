532b8512c6915123850cca095b9c75cf9ac28cb6
Jieun Jeon <jieun5230@naver.com>
2022-04-08 21:30:39 +0900

    DarkTheme using reactive variables and styledcomponents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 
+const lightTheme = {
+	fontColor: "#2c2c2c",
+	bgColor: "lightgray",
+};
+
+const darkTheme = {
+	fontColor: "lightgray",
+	bgColor: "#2c2c2c",
+};
+
 function App() {
 	const isLoggedIn = useReactiveVar(isLoggedInVar);
+	const darkMode = useReactiveVar(darkModeVar);
 	return (
-		<div>
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
 			<Router>
 				<Routes>
 					<Route
@@ -19,7 +31,7 @@ function App() {
 					<Route path="*" element={<NotFound />}></Route>
 				</Routes>
 			</Router>
-		</div>
+		</ThemeProvider>
 	);
 }
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,21 +1,20 @@
 import styled from "styled-components";
-import { isLoggedInVar } from "../apollo";
+import { darkModeVar } from "../apollo";
 
 const Title = styled.h1`
-	color: greenyellow;
-	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-		Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	color: ${(props) => props.theme.fontColor};
 `;
 
 const Container = styled.div`
-	background-color: yellow;
+	background-color: ${(props) => props.theme.bgColor};
 `;
 
 function Login() {
 	return (
 		<Container>
 			<Title>Login</Title>
-			<button onClick={() => isLoggedInVar(true)}>Log in now!</button>
+			<button onClick={() => darkModeVar(true)}>To dark</button>
+			<button onClick={() => darkModeVar(false)}>To Light</button>
 		</Container>
 	);
 }
