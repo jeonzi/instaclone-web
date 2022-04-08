8e7396fa235e42dfc9602c24259d48c138cd62fa
Jieun Jeon <jieun5230@naver.com>
2022-04-08 20:31:35 +0900

    Router Setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Nomadecoder Instaclone Web Client
+
+-  [x] Setup Router
+-  [ ] Authentication
+-  [ ] Arch.
+-  [ ] Styles (Dark Mode!!!)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-	return <div></div>;
+	const isLoggedIn = false;
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route
+						exact
+						path="/home"
+						element={isLoggedIn ? <Home /> : <Login />}
+					></Route>
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
