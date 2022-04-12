a5a7a13108c1d687e4b1428cf6f876eb8a5c8c00
Jieun Jeon <jieun5230@naver.com>
2022-04-12 21:33:23 +0900

    Forms in React / useState

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -11,6 +11,7 @@ import Input from "../components/auth/Input";
 import FormBox from "../components/auth/FormBox";
 import BottomBox from "../components/auth/BottomBox";
 import routes from "../routes";
+import { useState } from "react";
 
 const FacebookLogin = styled.div`
 	color: #385285;
@@ -21,16 +22,42 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
+	const [username, setUsername] = useState("");
+	const [usernameError, setUsernameError] = useState("");
+	const onUsernameChange = (event) => {
+		setUsernameError("");
+		setUsername(event.target.value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (username === "") {
+			setUsernameError("Not empty plz");
+		}
+		if (username.length < 5) {
+			setUsernameError("too short!");
+		}
+	};
+
 	return (
 		<AuthLayout>
 			<FormBox>
 				<div>
 					<FontAwesomeIcon icon={faInstagram} size="3x" />
 				</div>
-				<form>
-					<Input type="text" placeholder="Username" />
+				<form onSubmit={handleSubmit}>
+					{usernameError}
+					<Input
+						onChange={onUsernameChange}
+						value={username}
+						type="text"
+						placeholder="Username"
+					/>
 					<Input type="password" placeholder="Password" />
-					<Button type="submit" value="Log in" />
+					<Button
+						type="submit"
+						value="Log in"
+						disabled={username === "" && username.length < 5}
+					/>
 				</form>
 				<Separator />
 				<FacebookLogin>
