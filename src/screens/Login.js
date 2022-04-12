3e66cea43c8f01dc079505855d83fcb28403e0a5
Jieun Jeon <jieun5230@naver.com>
2022-04-12 22:49:08 +0900

    React Hook Form

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,6 +12,7 @@ import FormBox from "../components/auth/FormBox";
 import BottomBox from "../components/auth/BottomBox";
 import routes from "../routes";
 import PageTitle from "../components/PageTitle";
+import { useForm } from "react-hook-form";
 
 const FacebookLogin = styled.div`
 	color: #385285;
@@ -22,6 +23,13 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
+	const { register, handleSubmit } = useForm(); //useState, setvalue, onchange를 모두 다 해줌
+	const onSubmitValid = (data) => {
+		console.log(data);
+	};
+	const onSubmitInvalid = (data) => {
+		console.log(data, "invalid");
+	};
 	return (
 		<AuthLayout>
 			<PageTitle title="Login" />
@@ -29,9 +37,24 @@ function Login() {
 				<div>
 					<FontAwesomeIcon icon={faInstagram} size="3x" />
 				</div>
-				<form>
-					<Input type="text" placeholder="Username" />
-					<Input type="password" placeholder="Password" />
+				<form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+					<Input
+						{...register("username", {
+							required: "Username is required",
+							minLength: 5,
+							pattern: {
+								message: "5~15자의 영문과 숫자만 사용 가능합니다.",
+								value: /^[a-z0-9]{1,15}$/g,
+							},
+						})}
+						type="text"
+						placeholder="Username"
+					/>
+					<Input
+						{...register("password")}
+						type="password"
+						placeholder="Password"
+					/>
 					<Button type="submit" value="Log in" />
 				</form>
 				<Separator />
